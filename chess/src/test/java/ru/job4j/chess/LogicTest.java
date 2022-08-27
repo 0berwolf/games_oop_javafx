35be17a2821018975c613592e6b3c8f0b4232400
0berwolf <werewolf01051990@gmail.com>
2022-08-27 17:13:25 +0300

    Fix CI bot issues

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
@@ -8,10 +7,8 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-    @Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
-   
     @Test
     public void whenMoveThenFigureNotFoundException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
@@ -19,6 +16,34 @@ public class LogicTest {
         FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
             logic.move(Cell.C1, Cell.H6);
         });
+        logic.add(new BishopBlack(Cell.A1));
+        logic.add(new BishopBlack(Cell.B2));
+        logic.move(Cell.C3, Cell.A1);
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+
+    @Test
+    public void whenMoveThenImpossibleMove()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        logic.add(new BishopBlack(Cell.A1));
+        logic.move(Cell.A1, Cell.A3);
+        assertThat(exception.getMessage()).isEqualTo("Figure try impossible move.");
+    }
+
+    @Test
+    public void whenMoveThenOccupied()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        logic.add(new BishopBlack(Cell.A1));
+        logic.add(new BishopBlack(Cell.B2));
+        logic.move(Cell.B2, Cell.A1);
+        assertThat(exception.getMessage()).isEqualTo("Figure don't try move.");
     }
 }
